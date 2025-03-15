aa41dbe594aa41b87ccb04cffabec35081b83716
Illia Bobyr <illia.bobyr@gmail.com>
2025-03-14 19:52:44 -0700

    RpcClient::send_with_payer_latest_blockhash_with_spinner_and_config
    Useful when I want to force send transactions that fail simulation.
    Mostly for debugging.

--- src/rpc_client_ext.rs
+++ src/rpc_client_ext.rs
@@ -3,6 +3,7 @@
 use anyhow::{Context as _, Result};
 use solana_program::pubkey::Pubkey;
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
+use solana_rpc_client_api::config::RpcSendTransactionConfig;
 use solana_sdk::{
     commitment_config::CommitmentConfig, instruction::Instruction, signature::Signature,
     signer::signers::Signers, transaction::Transaction,
@@ -15,6 +16,16 @@ pub trait RpcClientExt {
         payer: Option<&Pubkey>,
         signing_keypairs: &SigningKeyparis,
     ) -> Result<Signature>;
+
+    async fn send_with_payer_latest_blockhash_with_spinner_and_config<
+        SigningKeyparis: Signers + ?Sized,
+    >(
+        &self,
+        instructions: &[Instruction],
+        payer: Option<&Pubkey>,
+        signing_keypairs: &SigningKeyparis,
+        config: RpcSendTransactionConfig,
+    ) -> Result<Signature>;
 }
 
 impl RpcClientExt for RpcClient {
@@ -23,6 +34,24 @@ impl RpcClientExt for RpcClient {
         instructions: &[Instruction],
         payer: Option<&Pubkey>,
         signing_keypairs: &SigningKeyparis,
+    ) -> Result<Signature> {
+        self.send_with_payer_latest_blockhash_with_spinner_and_config(
+            instructions,
+            payer,
+            signing_keypairs,
+            RpcSendTransactionConfig::default(),
+        )
+        .await
+    }
+
+    async fn send_with_payer_latest_blockhash_with_spinner_and_config<
+        SigningKeyparis: Signers + ?Sized,
+    >(
+        &self,
+        instructions: &[Instruction],
+        payer: Option<&Pubkey>,
+        signing_keypairs: &SigningKeyparis,
+        config: RpcSendTransactionConfig,
     ) -> Result<Signature> {
         // When the RpcClient is configured with a commitment that is not Finalized, I often see
         // "Blockhash not found" errors.  Considering that we run transactions right away, there
@@ -39,8 +68,12 @@ impl RpcClientExt for RpcClient {
             latest_blockhash,
         );
 
-        self.send_and_confirm_transaction_with_spinner(&transaction)
-            .await
-            .context("Transaction execution failed")
+        self.send_and_confirm_transaction_with_spinner_and_config(
+            &transaction,
+            self.commitment(),
+            config,
+        )
+        .await
+        .context("Transaction execution failed")
     }
 }
