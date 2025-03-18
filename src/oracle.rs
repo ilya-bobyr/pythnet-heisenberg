ef1b30a4de4b70000e924c8d6a4a0ff59e4b3555
Illia Bobyr <illia.bobyr@gmail.com>
2025-03-17 23:22:41 -0700

    New command: oracle/add-product

--- src/oracle.rs
+++ src/oracle.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 
 use crate::args::oracle::Command;
 
+mod add_product;
 mod init_mapping;
 pub mod instructions;
 mod update_permissions;
@@ -10,5 +11,9 @@ pub async fn run(command: Command) -> Result<()> {
     match command {
         Command::UpdatePermissions(args) => update_permissions::run(args).await,
         Command::InitMapping(args) => init_mapping::run(args).await,
+        Command::AddProduct(args) => {
+            args.check_are_valid()?;
+            add_product::run(args).await
+        }
     }
 }
