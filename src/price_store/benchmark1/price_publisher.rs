bb498182e140432ff161bd3f479d4c132b9c19db
Illia Bobyr <illia.bobyr@gmail.com>
2025-03-28 00:19:13 -0700

    WIP: Send via RPC instead of UDP

--- src/price_store/benchmark1/price_publisher.rs
+++ src/price_store/benchmark1/price_publisher.rs
@@ -12,6 +12,7 @@ use futures::{
 };
 use log::warn;
 use solana_program::{hash::Hash, pubkey::Pubkey};
+use solana_rpc_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::{
     clock::NUM_CONSECUTIVE_LEADER_SLOTS, signature::Keypair, signer::Signer as _,
     transaction::Transaction,
@@ -22,13 +23,17 @@ use tokio_util::sync::CancellationToken;
 use crate::{
     blockhash_cache::BlockhashCache,
     node_address_service::NodeAddressService,
-    price_store::instructions::submit_prices::{self, BufferedPrice, TradingStatus},
+    price_store::{
+        benchmark1::ResultIntoPriceUpdateResult as _,
+        instructions::submit_prices::{self, BufferedPrice, TradingStatus},
+    },
 };
 
 use super::{PriceUpdateResult, price_source::PriceSource};
 
 #[allow(clippy::too_many_arguments)]
 pub async fn run_publisher(
+    rpc_client: &RpcClient,
     program_id: Pubkey,
     payer: Keypair,
     publisher: Keypair,
@@ -88,6 +93,7 @@ pub async fn run_publisher(
         node_address_service.get_tpu_for_next_in_schedule(&mut target_nodes, fanout_slots.into());
 
         start_all_price_updates(
+            rpc_client,
             &mut pending_price_updates,
             &send_socket,
             latest_blockhash,
@@ -139,7 +145,8 @@ pub async fn run_publisher(
 type PriceUpdateFutures<'env> = FuturesUnordered<BoxFuture<'env, PriceUpdateResult>>;
 
 #[allow(clippy::too_many_arguments)]
-fn start_all_price_updates<'update_deps, 'socket: 'update_deps>(
+fn start_all_price_updates<'update_deps, 'rpc_client: 'update_deps, 'socket: 'update_deps>(
+    rpc_client: &'rpc_client RpcClient,
     price_updates: &mut PriceUpdateFutures<'update_deps>,
     socket: &'socket UdpSocket,
     latest_blockhash: Hash,
@@ -181,30 +188,140 @@ fn start_all_price_updates<'update_deps, 'socket: 'update_deps>(
             latest_blockhash,
         );
 
+        //- println!(
+        //-     "D.start_all_price_updates.1: starting task to rpc_send() from {}",
+        //-     publisher_pubkey
+        //- );
+        price_updates.push({
+            let transaction = transaction.clone();
+            Box::pin(async move {
+                // let rpc_result = rpc_client.send_transaction(&transaction).await;
+                debug_rpc_send(rpc_client, &transaction)
+                    .await
+                    .into_price_update_result()
+            })
+        });
+
+        const SEND_OVER_UDP: bool = false;
+        if !SEND_OVER_UDP {
+            continue;
+        }
+
         let buf = encode_to_vec(transaction, bincode::config::legacy())
             .context("Serialization of the submit prices transaction")?;
         for node_address in target_nodes.iter().copied() {
+            //- println!(
+            //-     "D.start_all_price_updates.2: starting task to send_to({}) to {}",
+            //-     buf.len(),
+            //-     node_address
+            //- );
             price_updates.push({
                 let buf = buf.clone();
                 Box::pin(async move {
+                    //- println!(
+                    //-     "D.start_all_price_updates.2.1: Socket local address pre send_to(): {:?}",
+                    //-     socket.local_addr(),
+                    //- );
+                    // let update_result = match socket.send_to(&buf, node_address).await {
                     match socket.send_to(&buf, node_address).await {
                         Ok(sent) => {
                             if sent != buf.len() {
                                 warn!("Failed to send a submit price transaction in one packet");
+                                //- println!(
+                                //-     "D.start_all_price_updates.2.2: send_to() cut from {} to {} bytes",
+                                //-     buf.len(),
+                                //-     sent
+                                //- );
                                 PriceUpdateResult::Fail
                             } else {
+                                //- println!("D.start_all_price_updates.2.3: send_to() sent {sent} bytes");
                                 PriceUpdateResult::Success
                             }
                         }
-                        Err(_) => {
+                        Err(_err) => {
                             // We do not care if the send fails.  We are not going to retry it.
+                            //- println!("D.start_all_price_updates.2.4: send_to() failed: {err:?}");
                             PriceUpdateResult::Fail
                         }
                     }
+
+                    //- println!(
+                    //-     "D.start_all_price_updates.2.5: Socket local address post send_to(): {:?}",
+                    //-     socket.local_addr(),
+                    //- );
+
+                    // update_result
                 })
             });
         }
     }
 
     Ok(())
+}
+
+use base64::{Engine, prelude::BASE64_STANDARD};
+use serde_json::json;
+use solana_rpc_client::rpc_client::SerializableTransaction;
+use solana_rpc_client_api::{
+    client_error::{ErrorKind as ClientErrorKind, Result as ClientResult},
+    config::RpcSendTransactionConfig,
+    request::{RpcError, RpcRequest, RpcResponseErrorData},
+    response::RpcSimulateTransactionResult,
+};
+use solana_sdk::signature::Signature;
+use solana_transaction_status::UiTransactionEncoding;
+
+async fn debug_rpc_send(
+    rpc_client: &RpcClient,
+    transaction: &Transaction,
+) -> ClientResult<Signature> {
+    let config = RpcSendTransactionConfig {
+        encoding: Some(UiTransactionEncoding::Base64),
+        preflight_commitment: Some(rpc_client.commitment().commitment),
+        ..RpcSendTransactionConfig::default()
+    };
+
+    let serialized_encoded = {
+        let serialized = encode_to_vec(transaction, bincode::config::legacy())
+            .expect("Transaction serialization failed");
+        //- println!("D.debug_rpc_send. tx byte len: {}", serialized.len());
+        BASE64_STANDARD.encode(serialized)
+    };
+
+    let _signature_base58_str: String = match rpc_client
+        .send(
+            RpcRequest::SendTransaction,
+            json!([serialized_encoded, config]),
+        )
+        .await
+    {
+        Ok(signature_base58_str) => signature_base58_str,
+        Err(err) => {
+            if let ClientErrorKind::RpcError(RpcError::RpcResponseError {
+                code,
+                message,
+                data,
+            }) = &err.kind
+            {
+                println!("{} {}", code, message);
+                if let RpcResponseErrorData::SendTransactionPreflightFailure(
+                    RpcSimulateTransactionResult {
+                        logs: Some(logs), ..
+                    },
+                ) = data
+                {
+                    for (i, log) in logs.iter().enumerate() {
+                        println!("{:>3}: {}", i + 1, log);
+                    }
+                    println!();
+                }
+            }
+            //- println!("D.debug_rpc_send: RPC failed: {err:?}");
+            return Err(err);
+        }
+    };
+
+    //- println!("D.debug_rpc_send: Tx RPC signature: {signature_base58_str}");
+
+    Ok(*transaction.get_signature())
 }
